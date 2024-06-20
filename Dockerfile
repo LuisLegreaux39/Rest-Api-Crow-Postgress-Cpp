8cc55f8fdced4b4644b1f81df14c14225247dff0
Luis Ricardo Legreaux Medina <59577641+LuisLegreaux39@users.noreply.github.com>
2024-06-20 16:58:53 -0400

    :rocket:

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM  conanio/gcc11-ubuntu16.04:latest
+
 # Set the working directory to /app inside the container
 WORKDIR /app
-# Copy app files
+
+# Switch to root user
+USER root
+
 COPY . .
 # Install dependencies ()
 CMD ["sh" ,"build.sh"]
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 export DB_HOST=db DB_NAME=cpp_library DB_USER=postgres DB_PASSWORD=postgres
+conan profile detect
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
